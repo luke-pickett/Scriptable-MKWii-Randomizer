d6c56b7c20ad151370f2b1270b5e2dae2af5d8fd
engooeer <engooeer@gmail.com>
2022-11-28 11:30:07 -0700

    Added Text

--- MK_Wii_Randomizer.js
+++ MK_Wii_Randomizer.js
@@ -1,5 +1,9 @@
 const widget = new ListWidget();
 
+let text = widget.addText("Test");
+text.font = Font.systemFont(36);
+text.textColor = color.white();
+
 Script.setWidget(widget);
 Script.complete();
 
