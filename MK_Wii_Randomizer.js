e4fa69e7e84fedbf67095776a84630469892e37c
engooeer <engooeer@gmail.com>
2022-11-28 12:10:28 -0700

    Added rows Fixed bug in input

--- MK_Wii_Randomizer.js
+++ MK_Wii_Randomizer.js
@@ -1,17 +1,24 @@
 const widget = new ListWidget();
+const mapList = [1,2,3,4,5,6,7,8,9,10]
 
+// Sets a int input from the user //
 let textField = new Alert();
 textField.title = "How many races?";
 textField.addAction("OK");
-let numField = textField.addTextField(8);
+let numField = textField.addTextField("8");
 numField.setNumberPadKeyboard()
-
 await textField.present();
 let input = textField.textFieldValue(0);
+///////////////////////////////////
 
-let text = widget.addText("Test");
-text.font = Font.systemFont(36);
-text.textColor = Color.white();
+for (i=0; i<parseInt(input, 10); i++) {
+    let row = widget.addStack();
+    row.layoutHorizontally();
+
+    let text = row.addText("Test");
+    text.font = Font.systemFont(36);
+    text.textColor = Color.white();
+}
 
 Script.setWidget(widget);
 Script.complete();
