976a9301e983dea9709fff02db4c38758d782286
engooeer <engooeer@gmail.com>
2022-11-28 11:45:07 -0700

    Added User Input

--- MK_Wii_Randomizer.js
+++ MK_Wii_Randomizer.js
@@ -1,8 +1,17 @@
 const widget = new ListWidget();
 
+let textField = new Alert();
+textField.title = "How many races?";
+textField.addCancelAction("Cancel");
+textField.addAction("OK");
+textField.addTextField("","");
+
+await textField.present();
+let input = textField.textFieldValue(0);
+
 let text = widget.addText("Test");
 text.font = Font.systemFont(36);
-text.textColor = color.white();
+text.textColor = Color.white();
 
 Script.setWidget(widget);
 Script.complete();
