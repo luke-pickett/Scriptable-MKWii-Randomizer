5260b69ed41205d405473c762738c471b44ad12b
engooeer <engooeer@gmail.com>
2022-11-29 01:02:32 -0700

    Adjusted outline to be less noticeable

--- MK_Wii_Randomizer.js
+++ MK_Wii_Randomizer.js
@@ -126,8 +126,8 @@ for (i=0; i<parseInt(input, 10); i++) {
     text.font = Font.boldSystemFont(20);
     text.minimumScaleFactor = 0.5
     text.textColor = cupDictionary[randomMap.split('- ').pop()];
-    text.shadowColor = Colors.white()
-    text.shadowRadius = 0.5
+    text.shadowColor = Color.white()
+    text.shadowRadius = 0.2
 }
 
 Script.setWidget(widget);
